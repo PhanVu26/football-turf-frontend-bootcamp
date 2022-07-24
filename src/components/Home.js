d0a41a6ff7251913d2eadad3acc15f51ebd0b746
Vu Van Phan <vuvphan@silverlakeaxis.com>
2022-07-24 10:23:45 +0700

    Add Big Calendar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,58 +1,86 @@
 
-const callouts = [
-  {
-    name: 'Desk and Office',
-    description: 'Work from home accessories',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-01.jpg',
-    imageAlt: 'Desk with leather desk pad, walnut desk organizer, wireless keyboard and mouse, and porcelain mug.',
-    href: '#',
-  },
-  {
-    name: 'Self-Improvement',
-    description: 'Journals and note-taking',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-02.jpg',
-    imageAlt: 'Wood table with porcelain mug, leather journal, brass pen, leather key ring, and a houseplant.',
-    href: '#',
-  },
-  {
-    name: 'Travel',
-    description: 'Daily commute essentials',
-    imageSrc: 'https://tailwindui.com/img/ecommerce-images/home-page-02-edition-03.jpg',
-    imageAlt: 'Collection of four insulated travel bottles on wooden shelf.',
-    href: '#',
-  },
-]
-function Home() {
-  return (
-    <div className="bg-gray-100">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="max-w-2xl mx-auto py-16 sm:py-24 lg:py-32 lg:max-w-none">
-          <h2 className="text-2xl font-extrabold text-gray-900">Collections</h2>
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-          <div className="mt-6 space-y-12 lg:space-y-0 lg:grid lg:grid-cols-3 lg:gap-x-6">
-            {callouts.map((callout) => (
-              <div key={callout.name} className="group relative">
-                <div className="relative w-full h-80 bg-white rounded-lg overflow-hidden group-hover:opacity-75 sm:aspect-w-2 sm:aspect-h-1 sm:h-64 lg:aspect-w-1 lg:aspect-h-1">
-                  <img
-                    src={callout.imageSrc}
-                    alt={callout.imageAlt}
-                    className="w-full h-full object-center object-cover"
-                  />
-                </div>
-                <h3 className="mt-6 text-sm text-gray-500">
-                  <a href={callout.href}>
-                    <span className="absolute inset-0" />
-                    {callout.name}
-                  </a>
-                </h3>
-                <p className="text-base font-semibold text-gray-900">{callout.description}</p>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
+moment.locale('en-GB');
+const localizer = momentLocalizer(moment);
+
+const myEventsList = [
+
+  { 
+    start: new Date(2022, 6, 25, 13, 30), 
+    end: new Date(2022, 6, 25, 14, 30), 
+    allDay: false, 
+    title: "Vũ Phan - 0967139958 - Pendding" ,
+    colorEvento:'yellow'
+  },
+  { 
+    start: new Date(2022, 6, 25, 15, 30), 
+    end: new Date(2022, 6, 25, 16, 30), 
+    allDay: false, 
+    title: "Vũ Phan - 0967139958 - Pendding" ,
+    colorEvento:'yellow'
+  },
+  { 
+    start: new Date(2022, 6, 25, 17, 30), 
+    end: new Date(2022, 6, 25, 18, 30), 
+    allDay: false, 
+    title: "Vũ Phan - 0967139958 - Booked" ,
+    colorEvento:'blue',
+    color: 'white'
+  },
+  { 
+    start: new Date(2022, 6, 26, 15, 0), 
+    end: new Date(2022, 6, 26, 16, 0), 
+    allDay: false, 
+    title: "Huy - 0967139958 - Booked" ,
+    colorEvento:'blue',
+    color: 'white'
+  },
+  { 
+    start: new Date(2022, 6, 26, 17, 0), 
+    end: new Date(2022, 6, 26, 18, 0), 
+    allDay: false, 
+    title: "Huy - 0967139958 - Pendding" ,
+    colorEvento:'yellow'
+  },
+  { 
+    start: new Date(2022, 6, 26, 18, 0), 
+    end: new Date(2022, 6, 26, 19, 0), 
+    allDay: false, 
+    title: "Huy - 0967139958 - Booked" ,
+    colorEvento:'blue',
+    color: 'white'
+  },
+  { 
+    start: new Date(2022, 6, 27, 19, 0), 
+    end: new Date(2022, 6, 27, 20, 0), 
+    allDay: false, 
+    title: "Vũ Phan - 0967139958 - Pendding",
+    colorEvento:'yellow' 
+  }
+    
+
+];
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Calendar
+        localizer={localizer}
+        events={myEventsList}
+        style={{ height: 700 }}
+        step={15}
+        defaultView={'week'}
+        defaultDate={new Date()}
+        eventPropGetter={(myEventsList) => {
+          const backgroundColor = myEventsList.colorEvento ? myEventsList.colorEvento : 'blue';
+          const color = myEventsList.color ? myEventsList.color : 'black';
+          return { style: { backgroundColor ,color} }
+        }}
+      />
     </div>
   );
 }
-
-export default Home;
